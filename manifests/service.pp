890955a8895116e38f8d56f0d6b810812f12d609
Helen Griffiths <threepistons@users.noreply.github.com>
2019-03-22 15:11:54 +0000

    fix #1, change hostname variable in cronstub

--- manifests/service.pp
+++ manifests/service.pp
@@ -36,8 +36,7 @@ class msktutil::service inherits msktutil {
     mode    => '0755',
     owner   => $msktutil::user,
     path    => $msktutil::cronstub,
-    content => "#!/bin/sh\n${msktutil::msktutilpath} ${dashn} --auto-update --auto-update-interval ${msktutil::keytabreplace} --computer-name ${facts['networking']['hostname']}", # lint:ignore:140chars
+    content => "#!/bin/sh\n${msktutil::msktutilpath} ${dashn} --auto-update --auto-update-interval ${msktutil::keytabreplace} --computer-name ${msktutil::myhostname}", # lint:ignore:140chars
   }
 
 }
-
